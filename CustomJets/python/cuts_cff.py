f7373a0c5e873948bdc213d352ba3095789ca0bd
Simon Rothman <srothman@mit.edu>
2024-01-26 11:14:56 -0500

    Updated thresholds

--- CustomJets/python/cuts_cff.py
+++ CustomJets/python/cuts_cff.py
@@ -1,13 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
 recoThresholds = cms.PSet(
-    EM0thresholds =   cms.vdouble(0.0, 0.0, 0.0),
-    HAD0thresholds =  cms.vdouble(0.0, 0.0, 0.0),
-    HADCHthresholds = cms.vdouble(0.0, 0.0, 0.0),
-    ELEthresholds =   cms.vdouble(0.0, 0.0, 0.0),
-    MUthresholds =    cms.vdouble(0.0, 0.0, 0.0),
+    EM0thresholds =   cms.vdouble(1.0),
+    HAD0thresholds =  cms.vdouble(3.0),
+    HADCHthresholds = cms.vdouble(0.0),
+    ELEthresholds =   cms.vdouble(0.0),
+    MUthresholds =    cms.vdouble(0.0),
 
-    etaRegions = cms.vdouble(0.0, 1.0, 2.0, 3.0) 
+    etaRegions = cms.vdouble(0.0, 3.0)
+)
+
+recoVtxCuts = cms.PSet(
+    fromPVcut = cms.int32(0),
+    puppiCut = cms.double(0.5),
+    maxDZ = cms.double(0.2),
+    maxDXY = cms.double(0.2),
 )
 
 genThresholds = cms.PSet(
@@ -18,13 +25,6 @@ genThresholds = cms.PSet(
     MUthresholds =    cms.vdouble(0.0),
 
     etaRegions = cms.vdouble(0.0, 10.0)
-)
-
-recoVtxCuts = cms.PSet(
-    fromPVcut = cms.int32(0),
-    puppiCut = cms.double(0.5),
-    maxDZ = cms.double(0.2),
-    maxDXY = cms.double(0.2),
 )
 
 genVtxCuts = cms.PSet(
