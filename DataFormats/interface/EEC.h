2c8c7026b39ac094ac64b59b8ec8c47ae8cdd283
Simon Rothman <srothman@mit.edu>
2024-08-21 17:08:59 -0400

    Update backend

--- DataFormats/interface/EEC.h
+++ DataFormats/interface/EEC.h
@@ -2,88 +2,83 @@
 #define SROTHMAN_DATAFORMATS_EEC_H
 
 #include <boost/multi_array.hpp>
+#include "SRothman/EECs/src/fastStructs.h"
 
 struct EECresult{
-    using vecptr = std::shared_ptr<std::vector<double>>;
-    using array2ptr = std::shared_ptr<boost::multi_array<double, 2>>;
-    using array3ptr = std::shared_ptr<boost::multi_array<double, 3>>;
-    using array4ptr = std::shared_ptr<boost::multi_array<double, 4>>;
-
     unsigned iJet, iReco; 
 
-    std::vector<vecptr> proj; //orders 2, 3, 4, 5, 6 
     unsigned maxOrder;
-
     bool doRes3;
-    array3ptr res3;
-
     bool doRes4Shapes;
-    array4ptr res4shapes;
-
     bool doRes4Fixed;
-    array2ptr res4fixed;
     
+    std::array<std::shared_ptr<std::vector<double>>, 5> proj;
+    std::shared_ptr<boost::multi_array<double, 3>> res3;
+    std::shared_ptr<fastEEC::res4shapes<double>> res4shapes;
+
     EECresult() :
         iJet(9999), iReco(9999),
-        proj({nullptr, nullptr, nullptr, nullptr, nullptr}),
         maxOrder(0),
-        doRes3(false), res3(nullptr),
-        doRes4Shapes(false), res4shapes(nullptr),
-        doRes4Fixed(false), res4fixed(nullptr) {}
+        doRes3(false), 
+        doRes4Shapes(false), 
+        doRes4Fixed(false), 
+        proj({{nullptr, nullptr, nullptr, nullptr, nullptr}}),
+        res3(nullptr), 
+        res4shapes(nullptr) {}
 
     EECresult(unsigned iJet, unsigned iReco, 
               unsigned maxOrder,
               bool doRes3, 
               bool doRes4Shapes, bool doRes4Fixed,
-              vecptr p2, vecptr p3, vecptr p4, vecptr p5, vecptr p6,
-              array3ptr r3, array4ptr r4s, array2ptr r4f) :
+              std::array<std::shared_ptr<std::vector<double>>, 5> p,
+              std::shared_ptr<boost::multi_array<double, 3>> r3,
+              std::shared_ptr<fastEEC::res4shapes<double>> r4s) :
         iJet(iJet), iReco(iReco),
-        proj({p2, p3, p4, p5, p6}),
         maxOrder(maxOrder),
-        doRes3(doRes3), res3(r3),
-        doRes4Shapes(doRes4Shapes), res4shapes(r4s),
-        doRes4Fixed(doRes4Fixed), res4fixed(r4f) {}
-
+        doRes3(doRes3),
+        doRes4Shapes(doRes4Shapes), 
+        doRes4Fixed(doRes4Fixed),
+        proj(p),
+        res3(r3),
+        res4shapes(r4s) {}
 };
 
 struct EECtransfer{
-    using array2ptr = std::shared_ptr<boost::multi_array<double, 2>>;
-    using array4ptr = std::shared_ptr<boost::multi_array<double, 4>>;
-    using array6ptr = std::shared_ptr<boost::multi_array<double, 6>>;
-    using array8ptr = std::shared_ptr<boost::multi_array<double, 8>>;
-
     unsigned iReco, iGen;
     unsigned maxOrder;
-    std::vector<array2ptr> proj;
     bool doRes3;
-    array6ptr res3;
     bool doRes4Shapes;
-    array8ptr res4shapes;
     bool doRes4Fixed;
-    array4ptr res4fixed;
+
+    std::array<std::shared_ptr<boost::multi_array<double, 2>>, 5> proj;
+    std::shared_ptr<boost::multi_array<double, 6>> res3;
+    std::shared_ptr<fastEEC::res4shapes_transfer<double>> res4shapes;
+
+    EECtransfer() :
+        iReco(9999), iGen(9999),
+        maxOrder(0),
+        doRes3(false),
+        doRes4Shapes(false),
+        doRes4Fixed(false),
+        proj({{nullptr, nullptr, nullptr, nullptr, nullptr}}),
+        res3(nullptr),
+        res4shapes(nullptr) {}
 
     EECtransfer(unsigned iReco, unsigned iGen, 
                 unsigned maxOrder,
                 bool doRes3,
                 bool doRes4Shapes, bool doRes4Fixed,
-                array2ptr p2, array2ptr p3, array2ptr p4, 
-                array2ptr p5, array2ptr p6,
-                array6ptr r3, array8ptr r4s, array4ptr r4f) :
+                std::array<std::shared_ptr<boost::multi_array<double, 2>>, 5> p2,
+                std::shared_ptr<boost::multi_array<double, 6>> r3,
+                std::shared_ptr<fastEEC::res4shapes_transfer<double>> r4s) :
         iReco(iReco), iGen(iGen),
         maxOrder(maxOrder),
-        proj({p2, p3, p4, p5, p6}),
-        doRes3(doRes3), res3(r3),
-        doRes4Shapes(doRes4Shapes), res4shapes(r4s),
-        doRes4Fixed(doRes4Fixed), res4fixed(r4f) {}
-
-    EECtransfer() :
-        iReco(9999), iGen(9999),
-        maxOrder(0),
-        proj({nullptr, nullptr, nullptr, nullptr, nullptr}),
-        doRes3(false), res3(nullptr),
-        doRes4Shapes(false), res4shapes(nullptr),
-        doRes4Fixed(false), res4fixed(nullptr) {}
-
+        doRes3(doRes3), 
+        doRes4Shapes(doRes4Shapes),
+        doRes4Fixed(doRes4Fixed), 
+        proj(p2),
+        res3(r3),
+        res4shapes(r4s) {}
 };
 
 #endif
