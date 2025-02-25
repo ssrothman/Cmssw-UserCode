b8d498ece8aae58ddbed2e50ae627f22d718aa10
Simon Rothman <srothman@mit.edu>
2025-02-25 12:20:19 -0500

    Uniform rs4trnsfer config

--- EECs/python/EECRes4TransferProducer_cfi.py
+++ EECs/python/EECRes4TransferProducer_cfi.py
@@ -1,32 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-import bins
+from res4calculator_cfi import res4transfercalculator
 
 EECRes4TransferProducer = cms.EDProducer("EECRes4TransferProducer",
     genJets = cms.InputTag(""),
     recoJets = cms.InputTag(""),
     matches = cms.InputTag(""),
-    calculator = cms.PSet(
-        tolerance = cms.double(0.05),
-        tri_tolerance = cms.double(0.05),
-        normType = cms.string("RAWPT"),
-        bins_reco = cms.PSet(
-            R = cms.vdouble(bins.R_bins_reco),
-            r_dipole = cms.vdouble(bins.r_dipole_bins_reco),
-            c_dipole = cms.vdouble(bins.c_dipole_bins_reco),
-            r_tee = cms.vdouble(bins.r_tee_bins_reco),
-            c_tee = cms.vdouble(bins.c_tee_bins_reco),
-            r_triangle = cms.vdouble(bins.r_triangle_bins_reco),
-            c_triangle = cms.vdouble(bins.c_triangle_bins_reco)
-        ),
-        bins_gen = cms.PSet(
-            R = cms.vdouble(bins.R_bins_gen),
-            r_dipole = cms.vdouble(bins.r_dipole_bins_gen),
-            c_dipole = cms.vdouble(bins.c_dipole_bins_gen),
-            r_tee = cms.vdouble(bins.r_tee_bins_gen),
-            c_tee = cms.vdouble(bins.c_tee_bins_gen),
-            r_triangle = cms.vdouble(bins.r_triangle_bins_gen),
-            c_triangle = cms.vdouble(bins.c_triangle_bins_gen)
-        )
-    )
+    calculator = res4transfercalculator
 )
--- EECs/python/res4calculator_cfi.py
+++ EECs/python/res4calculator_cfi.py
@@ -9,8 +9,8 @@ c_dipole_bins_reco = np.linspace(0, np.pi/2, 21)
 r_tee_bins_reco = np.linspace(0, 1.0, 21)
 c_tee_bins_reco = np.linspace(0, np.pi/2, 21)
 
-r_triangle_bins_reco = np.linspace(0, 1.0, 21)
-c_triangle_bins_reco = np.linspace(0, 2*np.pi, 41)
+r_triangle_bins_reco = np.linspace(0, 2.0, 31)
+c_triangle_bins_reco = np.linspace(-np.pi, np.pi, 41)
 
 R_bins_gen = np.linspace(0, 1.0, 11)
 
@@ -20,21 +20,44 @@ c_dipole_bins_gen = np.linspace(0, np.pi/2, 21)
 r_tee_bins_gen = np.linspace(0, 1.0, 21)
 c_tee_bins_gen = np.linspace(0, np.pi/2, 21)
 
-r_triangle_bins_gen = np.linspace(0, 1.0, 21)
-c_triangle_bins_gen = np.linspace(0, 2*np.pi, 41)
+r_triangle_bins_gen = np.linspace(0, 2.0, 31)
+c_triangle_bins_gen = np.linspace(-np.pi, np.pi, 41)
 
 res4calculator = cms.PSet(
-        tolerance = cms.double(0.05),
-        tri_tolerance = cms.double(0.05),
-        normType = cms.string("RAWPT"),
-        bins = cms.PSet(
-            R = cms.vdouble(R_bins_reco),
-            r_dipole = cms.vdouble(r_dipole_bins_reco),
-            c_dipole = cms.vdouble(c_dipole_bins_reco),
-            r_tee = cms.vdouble(r_tee_bins_reco),
-            c_tee = cms.vdouble(c_tee_bins_reco),
-            r_triangle = cms.vdouble(r_triangle_bins_reco),
-            c_triangle = cms.vdouble(c_triangle_bins_reco)
-        )
+    tolerance = cms.double(0.05),
+    tri_tolerance = cms.double(0.05),
+    normType = cms.string("RAWPT"),
+    bins = cms.PSet(
+        R = cms.vdouble(R_bins_reco),
+        r_dipole = cms.vdouble(r_dipole_bins_reco),
+        c_dipole = cms.vdouble(c_dipole_bins_reco),
+        r_tee = cms.vdouble(r_tee_bins_reco),
+        c_tee = cms.vdouble(c_tee_bins_reco),
+        r_triangle = cms.vdouble(r_triangle_bins_reco),
+        c_triangle = cms.vdouble(c_triangle_bins_reco)
     )
+)
 
+res4transfercalculator = cms.PSet(
+    tolerance = cms.double(0.05),
+    tri_tolerance = cms.double(0.05),
+    normType = cms.string("RAWPT"),
+    bins_reco = cms.PSet(
+        R = cms.vdouble(R_bins_reco),
+        r_dipole = cms.vdouble(r_dipole_bins_reco),
+        c_dipole = cms.vdouble(c_dipole_bins_reco),
+        r_tee = cms.vdouble(r_tee_bins_reco),
+        c_tee = cms.vdouble(c_tee_bins_reco),
+        r_triangle = cms.vdouble(r_triangle_bins_reco),
+        c_triangle = cms.vdouble(c_triangle_bins_reco)
+    ),
+    bins_gen = cms.PSet(
+        R = cms.vdouble(R_bins_gen),
+        r_dipole = cms.vdouble(r_dipole_bins_gen),
+        c_dipole = cms.vdouble(c_dipole_bins_gen),
+        r_tee = cms.vdouble(r_tee_bins_gen),
+        c_tee = cms.vdouble(c_tee_bins_gen),
+        r_triangle = cms.vdouble(r_triangle_bins_gen),
+        c_triangle = cms.vdouble(c_triangle_bins_gen)
+    )
+)
