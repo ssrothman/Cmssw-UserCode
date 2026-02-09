b15a8533b34e4d78e56a1fb36e87b4009b40774a
Simon Rothman <srothman@mit.edu>
2026-02-09 14:10:57 -0500

    more detailed splitting info :)

--- Analysis/python/addDeltaPsi.py
+++ Analysis/python/addDeltaPsi.py
@@ -1,3 +1,4 @@
+from re import split
 import FWCore.ParameterSet.Config as cms
 from SRothman.Analysis.DeltaPsiProducer_cfi import DeltaPsiProducer
 from PhysicsTools.NanoAOD.common_cff import *
@@ -15,7 +16,20 @@ def addDeltaPsi(process, jetsname):
         extension=cms.bool(True),
         singleton = cms.bool(False),
         variables = cms.PSet(
-            deltaPsi = Var("deltaPsi", float, doc="Delta Psi of the hardest splitting", precision=-1),
+            splitting_pt1 = Var("pt1", float, precision=-1),
+            splitting_pt2 = Var("pt2", float, precision=-1),
+            splitting_pt3 = Var("pt3", float, precision=-1),
+            splitting_pt4 = Var("pt4", float, precision=-1),
+            splitting_pt5 = Var("pt5", float, precision=-1),
+            splitting_dR23 = Var('delta_R23', float, precision=-1),
+            splitting_z23 = Var('z23', float, precision=-1),
+            splitting_kt23 = Var('kt23', float, precision=-1),
+            splitting_phi23 = Var('phi23', float, precision=-1),
+            splitting_dR45 = Var('delta_R45', float, precision=-1),
+            splitting_z45 = Var('z45', float, precision=-1),
+            splitting_kt45 = Var('kt45', float, precision=-1),
+            splitting_phi45 = Var('phi45', float, precision=-1),
+            splitting_deltaPsi = Var('deltaPsi', float, precision=-1)
         ),
     ))
     
